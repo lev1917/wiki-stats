05540c55215f5d76ad33f5e490fca086d8d4cc88
lev1917 <lev.ryskin@mail.ru>
2016-04-18 11:37:02 +0400

    Update wiki_stats.py

--- wiki_stats.py
+++ wiki_stats.py
@@ -20,38 +20,68 @@ class WikiGraph:
         print('Загружаю граф из файла: ' + filename)
 
         with open(filename) as f:
-            (n, _nlinks) = (0, 0) # TODO: прочитать из файла
-            
+            (n, _nlinks) = (0, 0)
+            k=f.readline()
+            k=k.rstrip()
+            n,_nlinks=map(int, k.split())
             self._titles = []
+            print(n, _nlinks)
             self._sizes = array.array('L', [0]*n)
             self._links = array.array('L', [0]*_nlinks)
             self._redirect = array.array('B', [0]*n)
             self._offset = array.array('L', [0]*(n+1))
+            for i in range(n):
+                title=f.readline()
+                title=title.rstrip()
+                r=self._offset[i]
+                self._titles.append(title)
+                g=f.readline()
+                g=g.rstrip()
+                size, redirect,m=map(int,g.split())
+                self._sizes[i]=size
+                self._redirect[i]=redirect
 
-            # TODO: прочитать граф из файла
+                for y in range(m):
+                    d=f.readline()
+                    d=d.rstrip()
+                    d=int(d)
+                    self._links[r+y]=d
+                self._offset[i+1]=r+m
+
+
 
         print('Граф загружен')
 
     def get_number_of_links_from(self, _id):
-        pass
+        return self._offset[_id+1]-self._offset[_id]
 
     def get_links_from(self, _id):
-        pass
+        g=self._links[self._offset[_id]:self._offset[_id+1]]
+        return g
 
     def get_id(self, title):
-        pass
+        for i in range(len(self._titles)):
+            if self._titles==title:
+                return i
 
     def get_number_of_pages(self):
-        pass
+        return len(self._titles)-self._redirect.count(0)
 
     def is_redirect(self, _id):
-        pass
+        if self._redirect[_id]:
+            return True
+        else:
+            return False
+
+
 
     def get_title(self, _id):
-        pass
+        return self._titles[_id]
 
     def get_page_size(self, _id):
-        pass
+        return self._sizes[_id]
+
+
 
 
 def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
@@ -59,17 +89,66 @@ def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5,
     # TODO: нарисовать гистограмму и сохранить в файл
 
 
-if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print('Использование: wiki_stats.py <файл с графом статей>')
-        sys.exit(-1)
+wg = WikiGraph()
+wg.load_from_file('wiki_small.txt')
+min1=float('+inf')
+min_sul=0
+print('количество статей с перенаправлением',len(wg._redirect)-wg._redirect.count(0))
+for i in range(len(wg._sizes)):
+        if wg.get_number_of_links_from(i)==min1:
+            min_sul+=1
+        if wg.get_number_of_links_from(i)<min1:
+            min_sul=1
+            min1=wg.get_number_of_links_from(i)
 
-    if os.path.isfile(sys.argv[1]):
-        wg = WikiGraph()
-        wg.load_from_file(sys.argv[1])
-    else:
-        print('Файл с графом не найден')
-        sys.exit(-1)
 
-    # TODO: статистика и гистограммы
+print('минимальное количество ссылок из статьи',min1)
+print('Количесво ссылок с минимальным количеством ссылок',min_sul)
+min1=float('-inf')
+min_sul=0
+l=''
+print('количество статей с перенаправлением',len(wg._redirect)-wg._redirect.count(0))
+for i in range(len(wg._sizes)):
+        if wg.get_number_of_links_from(i)==min1:
+            min_sul+=1
+
+        if wg.get_number_of_links_from(i)>min1:
+            min_sul=1
+            min1=wg.get_number_of_links_from(i)
+            l=wg.get_title(i)
+
+print('максимальное количество ссылок из статьи',min1)
+print('количество статей с максимальным количеством ссылок',min_sul)
+print('статья с наибольшим количеством ссылок',l)
+sum=0
+
+for i in range(len(wg._sizes)):
+    sum+=int(wg.get_number_of_links_from(i))
+print('среднее количество ссылок в статье',sum/len(wg._sizes))
+
+
+
+min1=float('+inf')
+min_sul=0
+for i in range(len(wg._sizes)):
+        if wg.get_number_of_links_from(i)-wg._redirect[i]==min1:
+            min_sul+=1
+
+        if wg.get_number_of_links_from(i)-wg._redirect[i]<min1:
+            min_sul=1
+            min1=wg.get_number_of_links_from(i)
+
+print('минимальное количество ссылок на статью',min1)
+print('количество статей с минимальным количеством внешних ссылок',min_sul)
+Pe=max(wg._redirect)
+l=''
+suM=0
+for i in range(len(wg._redirect)):
+    if wg._redirect[i]==Pe:
+        l=wg.get_title(i)
+        suM+=1
+Sum=0
+for i in range(len(wg._redirect)):
+    Sum+=wg._redirect[i]
+print('среднее количество внешних перенаправлений на статью',Sum/len(wg._redirect))
